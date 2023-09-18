b2bdd5ad7abb485caae02f2194b5fe9775437fc8
Eorg1337 <ya.homutsk@yandex.ru>
2023-09-18 11:53:20 +0300

    review change

--- cypress/e2e/constructor-page/constr.cy.js
+++ cypress/e2e/constructor-page/constr.cy.js
@@ -2,13 +2,13 @@ import React from 'react';
 import ingredients from '../../fixtures/ingredients-test.json'
 import user from '../../fixtures/user-test.json'
 
-const my_ingredients = '[data-test="ingredients"]';
-const my_constructor = '[data-test="constructor"]';
-const my_buns = '[data-test="buns"]';
-const my_mains = '[data-test="mains"]';
-const my_sauces = '[data-test="sauces"]';
-const submit_order = '[data-test="submit-order"]';
-const close_modal = '[data-test="close-modal"]';
+const myIngredients = '[data-test="ingredients"]';
+const myConstructor = '[data-test="constructor"]';
+const myBuns = '[data-test="buns"]';
+const myMains = '[data-test="mains"]';
+const mySauces = '[data-test="sauces"]';
+const submitOrder = '[data-test="submit-order"]';
+const closeModal = '[data-test="close-modal"]';
 const myUrl = "http://localhost:3000";
 
 describe("burger-constructor", function () {
@@ -19,26 +19,26 @@ describe("burger-constructor", function () {
     cy.intercept("GET", "api/auth/user", user);
   });
   it("check all ingredients modal", () => {
-    cy.get(my_ingredients).each((elem) => {
+    cy.get(myIngredients).each((elem) => {
       cy.wrap(elem).click({ force: true });
       cy.wait(100);
-      cy.get(close_modal).click();
+      cy.get(closeModal).click();
     });
   });
   it("check all ingr dnd in constructor", () => {
-    cy.get(my_ingredients).each((elem) => {
+    cy.get(myIngredients).each((elem) => {
       cy.wrap(elem).trigger("dragstart");
-      cy.get(my_constructor).trigger("drop");
+      cy.get(myConstructor).trigger("drop");
     });
   });
   it("check submit order and reroute to login page", () => {
-    cy.get(my_buns).find(my_ingredients).first().trigger("dragstart");
-    cy.get(my_constructor).trigger("drop");
-    cy.get(my_sauces).find(my_ingredients).first().trigger("dragstart");
-    cy.get(my_constructor).trigger("drop");
-    cy.get(my_mains).find(my_ingredients).first().trigger("dragstart");
-    cy.get(my_constructor).trigger("drop");
-    cy.get(submit_order).click();
+    cy.get(myBuns).find(myIngredients).first().trigger("dragstart");
+    cy.get(myConstructor).trigger("drop");
+    cy.get(mySauces).find(myIngredients).first().trigger("dragstart");
+    cy.get(myConstructor).trigger("drop");
+    cy.get(myMains).find(myIngredients).first().trigger("dragstart");
+    cy.get(myConstructor).trigger("drop");
+    cy.get(submitOrder).click();
     cy.location("pathname").should("eq", "/login");
   });
 });
